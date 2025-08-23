8cac414837abd9679597ac8ab52e63b3a78bb44e
aiden38335 <aiden38335@gmail.com>
2025-08-23 17:41:32 +0900

    visca barca
    it is a aura detecting system based on barca players from 2024~2025 season

---
+++ app.py
@@ -0,0 +1,39 @@
+import random
+import requests
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/player')
+def get_player():
+    return jsonify({"player":"Lamine Yamal, Raphinha, Pedri, Lewandowski"})
+
+@app.route('/coupons')
+def get_coupons():
+    return jsonify({"coupons": "1% 할인, 2% 할인"})
+
+@app.route('/order', methods = ["post"])
+def order():
+    data = request.get_json()
+    name = data.get("name")
+    type = data.get("type")
+    aura = 0
+
+    aura = int(type)
+    coupon = data.get("coupon")
+    coupon =int(coupon)
+
+    aura = (1 -coupon/100) * aura
+
+    type_name = data.get("type_name")
+
+    return jsonify({"msg": f"{name} 님, welcome to the club, {type_name} visca barca. visca catalunia. total aura is{aura}"})
+
+if __name__ == '__main__':
+    # debug=True 모드는 개발 중에만 사용해야 합니다.
+    app.run(debug=True, port=5002)
